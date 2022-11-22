455ed4b2ea7eae5cfe38f0091e7fa62a438f0da1
María Agustina Cassi <agustinacassi@Ordenador-portatil-de-Maria.local>
2022-11-22 18:22:56 -0300

    Creation of DB model

---
+++ server/src/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const validator = require("validator");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validator.default.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+  },
+  country: {
+    type: String,
+  },
+  profession: {
+    type: Array,
+  },
+  description: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  language: {
+    type: Array,
+  },
+  preferences: {
+    type: Array,
+  },
+  assignedMentee: {
+    type: String,
+  },
+  assignedMentor: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
