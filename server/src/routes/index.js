762b00236d3126272d6390c59580ec1afd1f52c4
Franco Jalil <francojalil7@gmail.com>
2022-11-24 10:49:36 -0300

    add Register

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const authRouter = require("./auth");
 
-router.get("/", (req,res)=> res.send("Index"))
+router.get("/", (req, res) => res.send("Index"));
 //router.use("/user", userRouter);
+router.use("/auth", authRouter);
 
 module.exports = router;
