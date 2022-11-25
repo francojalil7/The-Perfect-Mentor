4a5aa83bdd797d8e0f488fb20e342c109fcef8f8
Franco Jalil <francojalil7@gmail.com>
2022-11-25 17:56:33 -0300

    saqué espacios

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -2,13 +2,9 @@ const {validateToken} = require("../utils/tokens");
 
 const validateUser = (req, res, next) => {
   const {token} = req.body;
-
   const  payload  = validateToken(token);
-
   req.user = payload;
-
   if (payload) return next();
-
   res.sendStatus(401);
 };
 
