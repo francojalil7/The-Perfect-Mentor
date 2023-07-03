5ed422849b8e46318c88b7e2387bd87a82d51ca5
Tini Cassi <89404026+agustinacassi@users.noreply.github.com>
2023-07-03 16:37:43 -0300

    Vercel Config

--- src/states/user.js
+++ src/states/user.js
@@ -11,7 +11,7 @@ export const setUser = createAction("SET_USER");
 export const signUpUser = createAsyncThunk("SIGNUPUSER", async (body) => {
   try {
     const response = await axios.post(
-      "http://localhost:5001/auth/register",
+      "https://the-perfect-mentor-backend.vercel.app/auth/register",
       body
     );
     let data = response.data;
@@ -34,7 +34,7 @@ export const signUpUser = createAsyncThunk("SIGNUPUSER", async (body) => {
 
 export const effectLogin = createAsyncThunk("PERSISTENCIA", async (body) => {
   try {
-    const response = await fetch("http://localhost:5001/auth/login", {
+    const response = await fetch("https://the-perfect-mentor-backend.vercel.app/auth/login", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
