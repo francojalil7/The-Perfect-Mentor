3a586becad5f111bbfb313fa5901bd47dbe8da01
Agustina Cassi <89404026+agustinacassi@users.noreply.github.com>
2023-07-05 15:15:24 -0300

    Update SignInSection.js

--- src/sections/SignInSection.js
+++ src/sections/SignInSection.js
@@ -124,12 +124,12 @@ const SignInSection = () => {
                 errors.password.type === "maxLength" &&
                 errorMessage(maxLength)}
 
-              <a href="http://localhost:3000/forgotpass">
+              <a href=`${process.env.REACT_APP_BACKEND_URI}/forgotpass`>
                 <Text>Forgot password?</Text>
               </a>
               <Text>
                 Don't have an account?{" "}
-                <a href="http://localhost:3000/signUp">Sign up</a>
+                <a href=`${process.env.REACT_APP_BACKEND_URI}/signUp`>Sign up</a>
               </Text>
 
               <GreyButtonInside onClick={handleSubmit(onSubmit)}>
